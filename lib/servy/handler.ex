b56f2f8703a31b3fb286f0a2fa876afc97f71c6c
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-01-07 16:17:56 -0600

    More cleanup

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -70,23 +70,6 @@ defmodule Servy.Handler do
     %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
-  # def route(%{method: "GET", path: "/about"} = conv) do
-  #   file =
-  #     Path.expand("../../pages", __DIR__)
-  #     |> Path.join("about.html")
-
-  #   case File.read(file) do
-  #     {:ok, content} ->
-  #       %{conv | status: 200, resp_body: content}
-
-  #     {:error, :enoent} ->
-  #       %{conv | status: 404, resp_body: "File not found!"}
-
-  #     {:error, reason} ->
-  #       %{conv | status: 500, resp_body: "File error: #{reason}"}
-  #   end
-  # end
-
   def format_response(conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
